d47a69ec54c45bae3db415addf11dfe8be756c1b
christophe Valens <c.valens@spray.se>
2020-05-12 20:05:38 +0200

    first test case

--- frontend-test/cypress/integration/examples/regressionTestSuite.spec.js
+++ frontend-test/cypress/integration/examples/regressionTestSuite.spec.js
@@ -3,7 +3,16 @@
 
 describe('Regression',function(){
     it('valid login',function(){
-cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3000')
+    cy.url().should('eq','http://localhost:3000/login')
+    cy.get('h2').should('contain','Login')
+    cy.get("[type='text']").type('tester01').should('have.value','tester01')
+    cy.get("[type='password']").type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c').should('have.value','GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+    cy.get('.blue').click()
+    cy.get('.username').should('contain','Welcome tester01!')
+    cy.get('.user > .btn').should('be.visible').click()
+    cy.get('h2').should('contain','Login')
+    cy.get('.router-link-active').should('contain', 'Tester Hotel')
     })
 
 })
